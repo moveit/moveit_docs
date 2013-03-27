adc594a8747651c6e3c5495c4a35cfa0c308e63b
Dave Coleman <davetcoleman@gmail.com>
2013-03-27 15:00:47 -0700

    Cropped images and added script to auto-crop them in the future

---
+++ diagram/run_crop_moveit_api_diagram.sh
@@ -0,0 +1,5 @@
+# to get image dimensions
+#identify <image_name> 
+
+# crop image to saved size:
+mogrify -crop 1056x1500+0+0 moveit_api_diagram.png
---
+++ diagram/run_crop_planning_scene_monitor_diagram.sh
@@ -0,0 +1,5 @@
+# to get image dimensions
+#identify <image_name> 
+
+# crop image to saved size:
+mogrify -crop 1056x900+0+0 planning_scene_monitor_diagram.png 
